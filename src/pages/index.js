f3b24b00c44418f5987f855cbe1a0ceab9e70634
dgujs <jinsoo8457@dongguk.edu>
2020-01-26 17:04:49 +0900

    graphql_add

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,35 +3,12 @@ import Layout from "../components/layout"
 
 export default () => (
   <Layout>
-    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
-    <p>
-      What do I like to do? Lots of course but definitely enjoy building
-      websites.
-    </p>
+    <h1>Amazing Pandas Eating Things</h1>
+    <div>
+      <img
+        src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
+        alt="Group of pandas eating bamboo"
+      />
+    </div>
   </Layout>
 )
-
-// import React from "react"
-// import { Link } from "gatsby"
-// import Header from "../components/header"
-
-// export default () => (
-//   <div style={{ color: `purple` }}>
-//     <Link to="/contact/">Contact</Link>
-//     <Header headerText="Hello Gatsby!" />
-//     <p>What a world.</p>
-//     <img src="https://source.unsplash.com/random/400x200" alt="" />
-//   </div>
-// )
-
-// import React from "react"
-
-// export default () => (
-//   <div>
-//     <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
-//     <p>
-//       What do I like to do? Lots of course but definitely enjoy building
-//       websites.
-//     </p>
-//   </div>
-// )
