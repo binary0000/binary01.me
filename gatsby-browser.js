ac5ba02eff5cdefdcf45e5183f09f3435553ddba
dgujs <jinsoo8457@dongguk.edu>
2020-01-26 16:17:48 +0900

    React Set

---
+++ gatsby-browser.js
@@ -0,0 +1,8 @@
+/*
+just know that gatsby-browser.js is one of a handful of special files that Gatsby looks for and uses (if they exist). Here, the naming of the file is important. 
+*/
+
+import "./src/styles/global.css"
+
+// or:
+// require('./src/styles/global.css')
