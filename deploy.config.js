e2823aed5c8b2528abb41dddb28a5816edb69123
dgujs <binary01@kakao.com>
2020-02-21 15:32:55 +0900

    deploy_modify

--- deploy.config.js
+++ deploy.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   name: 'binary01.me',
-  script: 'serve',
-  env: {
-    PM2_SERVE_PATH: './public/',
-    PM2_SERVE_PORT: 80,
-    PM2_SERVE_HOMEPAGE: './public/index.html'
-  }
-}
+  script: 'npx',
+  interpreter: 'none',
+  args: 'serve -p 80'
+};
