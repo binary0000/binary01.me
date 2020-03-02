b3fde1e42c2485dcaa5b2faf774df6285dcb8a11
dgujs <jinsoo8457@dongguk.edu>
2020-03-02 23:31:20 +0900

    naver_meta_modi

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -28,6 +28,7 @@ const Layout = ({
       <Helmet>
         <html lang="en" />
         <title>{title}</title>
+        <meta name="naver-site-verification" content="844572e18c17cf3506d5b507fc501a1cdc568bfd"/>
         <meta name="description" content={description} />
         <meta property="og:site_name" content={title} />
         <meta property="og:image" content={metaImageUrl} />
@@ -36,7 +37,6 @@ const Layout = ({
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={metaImageUrl} />
         <meta name="google-site-verification" content="CyDWPc0fC2ZfuEHUOMD3kOr6Nzfhx5kKutQPio47bjQ" />
-        <meta name="naver-site-verification" content="844572e18c17cf3506d5b507fc501a1cdc568bfd"/>
         {/* google search engine register */}
       </Helmet>
       {children}
