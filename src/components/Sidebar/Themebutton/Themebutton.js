80a69f53bfe36e7026a28439a75d971ebfa3c28c
dgujs <jinsoo8457@dongguk.edu>
2020-06-08 23:28:08 +0900

    end

--- src/components/Sidebar/Themebutton/Themebutton.js
+++ src/components/Sidebar/Themebutton/Themebutton.js
@@ -1,36 +1,17 @@
 import React, { useContext } from 'react';
-import { ThemeToggler } from 'gatsby-plugin-dark-mode';
 import DarkModeToggle from 'react-dark-mode-toggle';
 import ThemeContext from '../../../context/ThemeContext';
 
 
 const Themebutton = () => {
-    // const [isDarkMode, setIsDarkMode] = useState(false);
     const { isDarkMode, setDarkMode } = useContext(ThemeContext);
 
-    console.log(document.body.className);
-
     return (
-
-        <DarkModeToggle onChange={() =>
-            setDarkMode()
-        }
+        <DarkModeToggle
+            onChange={() => setDarkMode()}
             checked={isDarkMode}
             size={75}
         />
-
-        // <ThemeToggler>
-        //     {({ toggleTheme }) =>
-        //         <DarkModeToggle onChange={() => 
-        //             setDarkMode()
-        //             // toggleTheme(isDarkMode ? 'dark' : 'light');
-
-        //         }
-        //             checked={isDarkMode}
-        //             size={75}
-        //         />
-        //     }
-        // </ThemeToggler>
     );
 };
 
